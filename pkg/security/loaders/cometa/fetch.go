b65fb4bb26b1cacb389f70f567fe0e510b7e9f53
Andrea Nanni <andrea.nanni3@gmail.com>
2024-11-24 15:58:10 +0100

    update cometa

--- pkg/security/loaders/cometa/fetch.go
+++ pkg/security/loaders/cometa/fetch.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	cometaURLTemplate = "https://www.cometafondo.it/quota/%s"
+	cometaURLTemplate = "https://www.cometafondo.it/andamenti/%s"
 )
 
 type parsedData struct {
@@ -26,32 +26,25 @@ func fetchData(isin string) ([]parsedData, error) {
 
 	data := []parsedData{}
 
-	c.OnHTML("div.content", func(e *colly.HTMLElement) {
-		e.ForEach("table.table-values", func(i int, e *colly.HTMLElement) {
-			e.ForEach("tbody tr", func(i int, e *colly.HTMLElement) {
-				if i == 0 {
-					return
-				}
+	c.OnHTML("#table_2", func(e *colly.HTMLElement) {
+		e.ForEach("tbody tr", func(i int, e *colly.HTMLElement) {
+			dateString, valueString := parseRowText(e.ChildTexts("td"))
 
-				yearString, monthString, valueString := parseRowText(e.ChildTexts("td"))
+			date, err := monday.ParseInLocation("01/2006", dateString, time.UTC, monday.LocaleItIT)
+			if err != nil {
+				log.Warnf("Error parsing date: %s", err)
+				return
+			}
 
-				date, err := monday.ParseInLocation("Jan 2006", monthString+" "+yearString, time.UTC, monday.LocaleItIT)
-				if err != nil {
-					log.Warnf("Error parsing date: %s", err)
-					return
-				}
+			closeQuote, err := strconv.ParseFloat(strings.ReplaceAll(valueString, ",", "."), 32)
+			if err != nil {
+				log.Warnf("Error parsing quote: %s", err)
+				return
+			}
 
-				valueString = strings.Split(valueString, " ")[0]
-				closeQuote, err := strconv.ParseFloat(strings.ReplaceAll(valueString, ",", "."), 32)
-				if err != nil {
-					log.Warnf("Error parsing quote: %s", err)
-					return
-				}
-
-				data = append(data, parsedData{
-					Date:       date.AddDate(0, 1, -1),
-					CloseQuote: closeQuote,
-				})
+			data = append(data, parsedData{
+				Date:       date.AddDate(0, 1, -1),
+				CloseQuote: closeQuote,
 			})
 		})
 	})
@@ -64,6 +57,6 @@ func fetchData(isin string) ([]parsedData, error) {
 	return data, nil
 }
 
-func parseRowText(values []string) (string, string, string) {
-	return values[0], values[1], values[2]
+func parseRowText(values []string) (string, string) {
+	return values[0], values[1]
 }
